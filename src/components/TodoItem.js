3457bbcf5d9b442f32e642d44a435b0a15eaa2bd
dra2a <63828961+dra2a@users.noreply.github.com>
2021-06-17 23:47:45 -0400

    updated styling for completed items

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,11 +5,16 @@ function TodoItem(props) {
     const text = props.item.text
     const completed = props.item.completed
     const id = props.item.id
+    const completedStyle = {
+        fontStyle: "italic",
+        color: "#cdcdcd",
+        textDecoration: "line-through"
+    }
 
     return (
         <div className="todo-item">
             <input type="checkbox" checked={completed} onChange={() => props.handleChange(id)}/>
-            <p>{text}</p>
+            <p style={completed ? completedStyle : null}>{text}</p>
         </div>
     )
 }
